1fbf111ca9d2a8e2314287f2061c41f82767fcfa
Dani Chagas <chagasdaniel788@gmail.com>
2024-05-30 00:29:45 -0300

    finish

--- src/Counter.java
+++ src/Counter.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
 
 public class Counter {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        try {
+            System.out.println("Digite o primeiro parâmetro");
+            int parameterOne = sc.nextInt();
+
+            System.out.println("Digite o segundo parâmetro");
+            int parameterTwo = sc.nextInt();
+
+
+
+            if (parameterOne >= parameterTwo) {
+                throw new InvalidParametersException("O segundo parâmetro deve ser maior que o primeiro!");
+            }
+
+            for (int i = 1; i <= parameterTwo - parameterOne; i++) {
+                System.out.println("Imprimindo o número " + i);
+            }
+
+        } catch (InvalidParametersException err) {
+            System.out.println(err.getMessage());
+        }
     }
 }
--- src/InvalidParametersException.java
+++ src/InvalidParametersException.java
@@ -1,2 +1,6 @@
-public class InvalidParametersException {
+public class InvalidParametersException extends Exception{
+    public InvalidParametersException(String message) {
+        super(message);
+    }
+
 }
